5b008693ed7b5b4b8f6bc4968b86c7873cfca5c5
Osama Sakhi <msakhi21@gmail.com>
2020-03-10 16:25:31 +0000

    Add pascal_ade20k script, must modify to get it running w/ pretrained resnet

---
+++ train_ade20k.sh
@@ -0,0 +1,10 @@
+python train.py \
+  --train_epochs 43 \
+  --batch_size 3 \
+  --weight_decay 2e-4 \
+  --model_dir models/ba=3,wd=2e-4,max_iter=300 \
+  --max_iter 300 \
+  --data_dir dataset/ade20k_pascal
+  #--debug
+  #--pre_trained_model pretrained \
+  #--debug
